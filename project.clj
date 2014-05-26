a27cdcf81bb80bcdb3851eaf5b6c707335e5bc9b
Kazunori Kajihiro <likerichie@gmail.com>
2014-05-26 17:56:35 +0900

    cleanup dependeincies

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject dokkaa-builder "0.1.0-SNAPSHOT"
+(defproject dokkaa-builder "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [environ "0.5.0"]
-                 [circleci/clj-yaml "0.5.2"]
                  [http-kit "2.1.18"]
-                 [compojure "1.1.8"]
                  [cheshire "5.3.1"]
-                 [clj-http "0.9.1"]
-                 [com.cemerick/friend "0.2.1"]
+                 [compojure "1.1.8"]
                  [clojurewerkz/urly "1.0.0"]
-                 [ring/ring-devel "1.2.2"]
-                 [ring "1.2.2"]
-                 [k2nr/docker "0.0.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
+                 [com.cemerick/friend "0.2.1"
+                  :exclusions [org.apache.httpcomponents/httpclient]]
+                 [k2nr/docker "0.0.2-SNAPSHOT"]
+                 [ring "1.2.2"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
+                                  [ring/ring-devel "1.2.2"]]}}
   :plugins [[lein-environ "0.5.0"]]
-  :main dokkaa-builder.core)
+  :main dokkaa-builder.core
+  :aot [dokkaa-builder.core])
