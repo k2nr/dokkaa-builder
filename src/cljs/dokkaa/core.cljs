1c76bb481a2acc03b6cfa0fba0c82db1704614a0
Kazunori Kajihiro <likerichie@gmail.com>
2014-06-05 22:13:29 +0900

    add cljs script and html(just for playground)

---
+++ src/cljs/dokkaa/core.cljs
@@ -0,0 +1,15 @@
+(ns dokkaa.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(defn widget [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/h1 nil (:text data)))))
+
+(om/root widget
+         {:text "Hello world!"}
+         {:target (.getElementById js/document "my-app")})
