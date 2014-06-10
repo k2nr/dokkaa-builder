8c4548141dceb289871198bf7f2703048762b983
Kazunori Kajihiro <likerichie@gmail.com>
2014-06-10 15:14:17 +0900

    wip: client-side page by om

--- src/cljs/dokkaa_builder/core.cljs
+++ src/cljs/dokkaa_builder/core.cljs
@@ -1,16 +1,39 @@
 (ns dokkaa-builder.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [clojure.browser.repl]
             [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]
+            [sablono.core :as html :refer-macros [html]]
+            [clojure.data :as data]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
-(defn widget [data owner]
+(def app-state
+  (atom {}))
+
+(defn side-menu-view [app owner]
   (reify
     om/IRender
-    (render [this]
-      (dom/h1 nil (:text data)))))
+    (render [_]
+      (html [:div {:id "side-menu"}
+             [:ul]]))))
 
-(om/root widget
-         {:text "Hello world!"}
-         {:target (.getElementById js/document "my-app")})
+(defn apps-view [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (html [:div {:id "main-view"}
+             [:ul]]))))
+
+(defn root-view [app-owner]
+  (reify
+    om/IRender
+    (render [_]
+      (html [:div
+             (om/build side-menu-view nil)
+             (om/build apps-view nil)]))))
+
+(om/root root-view app-state
+  {:target (. js/document (getElementById "app"))})
