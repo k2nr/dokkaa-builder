00e637d8ad54686b5cf49c6a7599146bae374238
Kazunori Kajihiro <likerichie@gmail.com>
2014-06-10 20:48:50 +0900

    remove dummy routes

--- src/clj/dokkaa_builder/route.clj
+++ src/clj/dokkaa_builder/route.clj
@@ -10,35 +10,6 @@
             [cemerick.friend :as friend]
             [dokkaa-builder.oauth.github :as github]))
 
-(declare render-status-page)
-(declare render-repos-page)
-(declare get-github-repos)
-
-(defn render-status-page [request]
-  (let [count (:count (:session request) 0)
-        session (assoc (:session request) :count (inc count))]
-    (-> (ring.util.response/response
-           (str "<p>We've hit the session page " (:count session)
-                " times.</p><p>The current session: " session "</p>"))
-         (assoc :session session))))
-
-(defn render-repos-page
-  "Shows a list of the current users github repositories by calling the github api
-   with the OAuth2 access token that the friend authentication has retrieved."
-  [request]
-  (prn request)
-  (let [access-token (get-in request [:session :cemerick.friend/identity :current :access-token])
-        repos-response (get-github-repos access-token)]
-    (str (vec (map :name repos-response)))))
-
-(defn get-github-repos
-  "Github API call for the current authenticated users repository list."
-  [access-token]
-  (let [url (str "https://api.github.com/user/repos?access_token=" access-token)
-        response (http/get url {:accept :json})
-        repos (j/parse-string (:body response) true)]
-    repos))
-
 (defn get-apps [req]
   {:status 200
    :body (j/encode {"apps1" {:id "123"}
@@ -94,18 +65,12 @@
   (PUT    "/:app" req (update-app req))
   (DELETE "/:app" req (delete-app req)))
 
-(defroutes users-routes
-  (GET "/" request "<a href=\"/users/repos\">My Github Repositories</a><br><a href=\"/users/status\">Status</a>")
-  (GET "/status" request (render-status-page request))
-  (GET "/repos"  request (friend/authorize #{:user} (render-repos-page request)))
-  (friend/logout (ANY "/logout" request (ring.util.response/redirect "/"))))
-
 (defroutes routes
   (GET "/" req (index req))
   (GET "/_ping"  [] ping)
-  (context "/users" req users-routes)
   (context "/apps/" req apps-routes)
   (resources "/")
+  (friend/logout (ANY "/logout" request (ring.util.response/redirect "/")))
   (not-found "404 Not Found"))
 
 (def app (-> routes
