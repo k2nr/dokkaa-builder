09e0c2aa410d16de2a8067ae3f0c4b3b2cbdb593
Kazunori Kajihiro <likerichie@gmail.com>
2014-06-13 04:30:24 +0900

    kill old container if exists

--- provision.sh
+++ provision.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 cd /home/core/share
+OLD_CONTAINER=$(docker ps |grep k2nr/dokkaa-builder | awk '{print $1}')
+if [[ -n $OLD_CONTAINER ]]; then
+    sudo docker kill $OLD_CONTAINER
+fi
 sudo docker build -t k2nr/dokkaa-builder . > /dev/null
 HOST_IP=$(ip -o -4 addr show docker0 | awk -F '[ /]+' '/global/ {print $4}')
 echo HOST_IP=$HOST_IP
